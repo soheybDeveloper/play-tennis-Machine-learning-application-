2beb09ac7a9ad70985e5385b62c566e79d3a2ec3
Farohe soheyb <95651766+soheybDeveloper@users.noreply.github.com>
2024-01-16 21:24:15 +0100

    Update decision_maker.py

--- pipline/decision_maker.py
+++ pipline/decision_maker.py
@@ -1,5 +1,5 @@
 import joblib
-# import os
+import os
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.pipeline import Pipeline
@@ -15,20 +15,30 @@ class Pipeline:
         if cls._pipeline is None:
             cls._pipeline= cls.load_model()
         return cls._pipeline
-
     @classmethod
     def get_proccessor(cls):
-        
+        try:
             if cls._proccessor is None:
-                proccessor_path = 'preprocessor.pkl'
+                current_folder = os.path.dirname(os.path.abspath(__file__))
+                proccessor_path = os.path.join(current_folder, 'preprocessor.pkl')
                 cls._proccessor = joblib.load(proccessor_path)
             return cls._proccessor
+        except FileNotFoundError as e:
+            print(f"FileNotFoundError: {e}")
+            print(f"Current files in the directory: {os.listdir(current_folder)}")
+            raise
 
     @classmethod
     def load_model(cls):
-            model_path = 'pipline/pipeline.pkl'
+        try:
+            current_folder = os.path.dirname(os.path.abspath(__file__))
+            model_path = os.path.join(current_folder, 'pipeline.pkl')
             pip = joblib.load(model_path)
             return pip
+        except FileNotFoundError as e:
+            print(f"FileNotFoundError: {e}")
+            print(f"Current files in the directory: {os.listdir(current_folder)}")
+            raise
 
     
     @classmethod
