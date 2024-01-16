d587b9120c5573c8dc555d316079da10f5fdb091
Farohe soheyb <95651766+soheybDeveloper@users.noreply.github.com>
2024-01-16 21:30:07 +0100

    Update decision_maker.py

--- pipline/decision_maker.py
+++ pipline/decision_maker.py
@@ -17,29 +17,28 @@ class Pipeline:
         return cls._pipeline
     @classmethod
     def get_proccessor(cls):
-        try:
-            if cls._proccessor is None:
-                current_folder = os.path.dirname(os.path.abspath(__file__))
-                proccessor_path = os.path.join(current_folder, 'preprocessor.pkl')
-                cls._proccessor = joblib.load(proccessor_path)
-            return cls._proccessor
-        except FileNotFoundError as e:
-            print(f"FileNotFoundError: {e}")
+        
+        if cls._proccessor is None:
+            current_folder = os.path.dirname(os.path.abspath(__file__))
+            
             print(f"Current files in the directory: {os.listdir(current_folder)}")
-            raise
+            proccessor_path = os.path.join(current_folder, 'preprocessor.pkl')
+            print(f"proccessor_path: {proccessor_path}")
+            cls._proccessor = joblib.load(proccessor_path)
+        return cls._proccessor
+
+           
+            
 
     @classmethod
     def load_model(cls):
-        try:
+
             current_folder = os.path.dirname(os.path.abspath(__file__))
             model_path = os.path.join(current_folder, 'pipeline.pkl')
-            print(model_path)
+    
             pip = joblib.load(model_path)
             return pip
-        except FileNotFoundError as e:
-            print(f"FileNotFoundError: {e}")
-            print(f"Current files in the directory: {os.listdir(current_folder)}")
-            raise
+     
 
     
     @classmethod
@@ -49,18 +48,18 @@ class Pipeline:
 
     @classmethod
     def visualize_tree(cls):
-        classifier = cls.get_pipeline().named_steps['classifier']
+        # classifier = cls.get_pipeline().named_steps['classifier']
         fig, ax = plt.subplots(figsize=(23, 19))
-        plot_tree(classifier,
-                  feature_names=cls.get_proccessor().get_feature_names_out(['outlook', 'temp', 'humidity', 'windy']),
-                  class_names=['No', 'Yes'],
-                  filled=True,
-                  rounded=True,
-                  impurity=False,
-                  proportion=True,
-                  precision=2,
-                  ax=ax,
-                  fontsize=16)
+        # plot_tree(classifier,
+        #           feature_names=cls.get_proccessor().get_feature_names_out(['outlook', 'temp', 'humidity', 'windy']),
+        #           class_names=['No', 'Yes'],
+        #           filled=True,
+        #           rounded=True,
+        #           impurity=False,
+        #           proportion=True,
+        #           precision=2,
+        #           ax=ax,
+        #           fontsize=16)
         ax.set_title("Decision Tree for Tennis Decision Making")
         return fig
       
